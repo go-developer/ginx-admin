e636b186f5154809bb42f7f8e934f19569c97386
张德满 <go_developer@163.com>
2020-10-31 21:50:25 +0800

    优化js引入

--- html/assets/js/common/common.js
+++ html/assets/js/common/common.js
@@ -1,3 +1,22 @@
+// 引入 include 标签解析
+document.write('<script src="/assets/js/common/include.js"></script>')
+
+// 引入基础依赖js
+document.write('<script src="/assets/js/jquery.min.js"></script>')
+document.write('< script src = "/assets/js/bootstrap.bundle.min.js" ></script >')
+document.write('<script src="/assets/js/metismenu.min.js"></script>')
+document.write('<script src="/assets/js/waves.js"></script>')
+document.write('<script src="/assets/js/feather.min.js"></script>')
+document.write('<script src="/assets/js/jquery.slimscroll.min.js"></script>')
+document.write('<script src="/assets/js/jquery-ui.min.js"></script>')
+
+document.write('<script src="/plugins/apexcharts/apexcharts.min.js"></script>')
+document.write('<script src="/assets/pages/jquery.helpdesk-dashboard.init.js"></script>')
+
+//App js
+document.write('<script src="/assets/js/jquery.cookie.js"></script>')
+document.write('<script src="/assets/js/app.js"></script>')
+
 // 公共js
 
 // 是否是开发模式
