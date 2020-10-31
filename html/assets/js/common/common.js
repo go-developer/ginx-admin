05e8bc9f93e460e4cec278804a19be149aae48fd
张德满 <go_developer@163.com>
2020-11-01 00:21:22 +0800

    scheme管理模版

--- html/assets/js/common/common.js
+++ html/assets/js/common/common.js
@@ -3,15 +3,12 @@ document.write('<script src="/assets/js/common/include.js"></script>')
 
 // 引入基础依赖js
 document.write('<script src="/assets/js/jquery.min.js"></script>')
-document.write('< script src = "/assets/js/bootstrap.bundle.min.js" ></script >')
+document.write('<script src ="/assets/js/bootstrap.bundle.min.js"></script>')
 document.write('<script src="/assets/js/metismenu.min.js"></script>')
 document.write('<script src="/assets/js/waves.js"></script>')
 document.write('<script src="/assets/js/feather.min.js"></script>')
 document.write('<script src="/assets/js/jquery.slimscroll.min.js"></script>')
 document.write('<script src="/assets/js/jquery-ui.min.js"></script>')
-
-document.write('<script src="/plugins/apexcharts/apexcharts.min.js"></script>')
-document.write('<script src="/assets/pages/jquery.helpdesk-dashboard.init.js"></script>')
 
 //App js
 document.write('<script src="/assets/js/jquery.cookie.js"></script>')
